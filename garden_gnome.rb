551f337cd89faf6cd6bf7b7291f582c41ff2cfed
meredithheller <meredithheller@outlook.com>
2018-08-13 13:56:17 +0000

    automatically backed up by learn

--- garden_gnome.rb
+++ garden_gnome.rb
@@ -30,4 +30,10 @@ class GardenGnome
   def gnaw
     return "Gnawing on a tree!!!"
   end
+  def shout
+    return "GNARLY!!!"
+  end
+  def introude_self
+    return "Hello humans, my name is #{name}, I am #{age} years old, and you'll rue the day you crossed me!"
+  end
 end
