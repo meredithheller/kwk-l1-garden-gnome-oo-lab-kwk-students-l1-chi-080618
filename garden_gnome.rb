800d099b3ce644950f6504e458372f0adbcae4a9
meredithheller <meredithheller@outlook.com>
2018-08-13 13:57:09 +0000

    automatically backed up by learn

--- garden_gnome.rb
+++ garden_gnome.rb
@@ -33,7 +33,7 @@ class GardenGnome
   def shout
     return "GNARLY!!!"
   end
-  def introude_self
+  def introduce_self
     return "Hello humans, my name is #{name}, I am #{age} years old, and you'll rue the day you crossed me!"
   end
 end
